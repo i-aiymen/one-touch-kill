4a356e508eb9660414439f0226d2563d9de4be2a
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2022-08-20 10:47:24 +0530

    Created and Updated: player function

---
+++ player.rb
@@ -0,0 +1,17 @@
+class Player < Sprite
+    attr_accessor :score
+    
+    def initialize(x,y,image)
+        @score = 0
+        super
+    end
+
+    def update 
+        self.x += Input.x 
+        self.y += Input.y 
+    end
+
+    def shot
+        @score += 1 
+    end
+end
